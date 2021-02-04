7337f05f0bd954a22d7611f0be63f7082c508259
nivig-15 <75109719+nivig-15@users.noreply.github.com>
2021-02-04 19:52:28 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -6,9 +6,9 @@ float h,d,b,vol;
 printf("The height of the tromboloid is: ");
 scanf("%f",&h);
 printf("The depth of the tromboloid is: ");
-scanf("%f",&d)
+scanf("%f",&d);
 printf("The breadth of the tromboloid is: ");
 scanf("%f",&b);
-vol=(0.33)*(h*d*b)+(d/b); //the value of 1/3 is approximately 0.33
+vol=(0.33)*((h*d*b)+(d/b)); //the value of 1/3 is approximately 0.33
 printf("The volume of the tromboloid is: %.2f",vol);
 }
